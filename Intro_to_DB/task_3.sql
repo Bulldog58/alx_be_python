4a78f8481fcff208fb5a541283fab9d9a4a94d69
Your Name <your.email@example.com>
2025-10-19 18:26:30 +0300

    SQL and Database management

---
+++ Intro_to_DB/task_3.sql
@@ -0,0 +1,4 @@
+-- Active: 1760887119542@@localhost@3306@alx_book_store
+-- Active: 1760884686331@@localhost@3306@mysql
+USE alx_book_store;  -- select the database
+SHOW TABLES;
---
+++ Intro_to_DB/task_5.sql
@@ -0,0 +1,5 @@
+-- Active: 1760887119542@@localhost@3306@alx_book_store
+
+INSERT INTO alx_book_store.Customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+-- This query inserts a new customer into the 'Customers' table
